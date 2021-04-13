2cf5e8d690e1dbdea0d88cb730c58545eabed3e9
gragkavanis <Gerasimos.Ragkavanis@dts-cloudsecurity.gr>
2021-04-13 10:58:10 +0200

    Testing Auth0 functionallity and options added config files and two routes in index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 // Middlewares
 // app.use((req, res, next) => {
@@ -20,8 +21,22 @@ var multer = require('multer');
 var upload = multer();
 app.use(upload.array());  // body-parser for form-data
 
-// Connection to db
+// Config dotenv file so it can be used below
 dotenv.config();
+
+// Auth0 middleware
+app.use(
+    auth({
+        authRequired: false,
+        auth0Logout: true,
+        issuerBaseURL: process.env.ISSUER_BASE_URL,
+        baseURL: process.env.BASE_URL,
+        clientID: process.env.CLIENT_ID,
+        secret: process.env.SECRET
+    })
+);
+
+// Connection to db
 try {
     mongoose.connect(process.env.LOCALHOST_DB,
         { useNewUrlParser: true }, () => {
@@ -30,7 +45,6 @@ try {
 } catch (error) {
     console.error("Failed to connect to database: ", error);
 }
-
 
 // Route middlewares
 const authentication = require('./routes/auth.js');
@@ -45,6 +59,14 @@ app.use('/api/products', products);
 const orders = require('./routes/orders.js');
 app.use('/api/orders', orders);
 
+// Auth0 functionallity
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Not authorized' );
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+});
 
 app.listen(3000, () => {
     console.log('Server up and running');
