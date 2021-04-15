5308ea329ee3285813eb74c21101a60fcc0743a3
gragkavanis <Gerasimos.Ragkavanis@dts-cloudsecurity.gr>
2021-04-15 09:26:14 +0200

    Added enviremoent variable. If it is production the console logs are disabled.

--- util/logger.js
+++ util/logger.js
@@ -1,14 +1,21 @@
 const winston = require('winston');
- 
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
-    new winston.transports.Console({ format: winston.format.simple() })
+    new winston.transports.File({ filename: 'combined.log' })
   ]
 });
+
+if (process.env.NODE_ENV != 'PRODUCTION'){
+  console.log("Not Production mode enable console logging");
+  logger.add(new winston.transports.Console({ format: winston.format.simple() }));
+}
 
 module.exports = logger;
 
